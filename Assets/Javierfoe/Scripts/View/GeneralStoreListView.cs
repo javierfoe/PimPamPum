08f2c630a82b708f8de13060479a9046ee0aaf95
JaviBrador <javier.foe@gmail.com>
2019-01-07 14:52:09 +0100

    Added GeneralStoreListView, GeneralStoreView and GeneralStorePrefab.

---
+++ Assets/Javierfoe/Scripts/View/GeneralStoreListView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Bang
+{
+    public class GeneralStoreListView : CardListView<IGeneralStoreCardView>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            gameObject.SetActive(false);
+        }
+
+        protected override GameObject GetPrefab()
+        {
+            return GameController.GeneralStorePrefab;
+        }
+
+        public void EnableCards(bool value)
+        {
+            foreach(IGeneralStoreCardView cv in list)
+            {
+                cv.Enable(value);
+            }
+        }
+    }
+}
---
+++ Assets/Javierfoe/Scripts/View/GeneralStoreView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+
+namespace Bang
+{
+    public class GeneralStoreView : CardView, IGeneralStoreCardView
+    {
+        private Button button;
+
+        public void Enable(bool value)
+        {
+            if (!button)
+            {
+                button = GetComponent<Button>();
+                button.onClick.AddListener(ClickGeneralStoreCard);
+            }
+            Playable(value);
+            button.interactable = value;
+        }
+
+        public override void Playable(bool value)
+        {
+            base.Playable(value);
+            Draggable = false;
+        }
+
+        private void ClickGeneralStoreCard()
+        {
+            PlayerController.LocalPlayer.ChooseGeneralStoreCard(index);
+        }
+
+    }
+}
