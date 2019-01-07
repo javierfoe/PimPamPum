2844a02067ea9d028297779524000b0a0e17b173
JaviBrador <javier.foe@gmail.com>
2019-01-07 11:09:40 +0100

    Gitignore updated. Both hostaddress and playername inputfields now save their values to playerprefs.

---
+++ Assets/Javierfoe/Scripts/UI/HostAdressInputField.cs
@@ -0,0 +1,11 @@
+﻿
+public class HostAdressInputField : MultiplayerLocal.InputField {
+
+    private const string hostAddressKey = "HostAddress";
+
+    protected override void Awake()
+    {
+        key = hostAddressKey;
+        base.Awake();
+    }
+}
---
+++ Assets/Javierfoe/Scripts/UI/InputField.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Input = UnityEngine.UI.InputField;
+
+namespace MultiplayerLocal
+{
+    [RequireComponent(typeof(Input))]
+    public class InputField : MonoBehaviour
+    {
+        protected string key;
+
+        public Input Input
+        {
+            get; private set;
+        }
+
+        // Use this for initialization
+        protected virtual void Awake()
+        {
+            Input = GetComponent<Input>();
+            if (PlayerPrefs.HasKey(key))
+            {
+                Input.text = PlayerPrefs.GetString(key);
+            }
+            Input.onValueChanged.AddListener(ValueChanged);
+        }
+
+        private void ValueChanged(string value)
+        {
+            PlayerPrefs.SetString(key, value);
+        }
+    }
+}
