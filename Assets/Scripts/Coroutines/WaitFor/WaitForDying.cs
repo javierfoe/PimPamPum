a013278587fe7e1d1a7c667c4cbf9727e7b93d36
Javier Frias <javier.foe@gmail.com>
2019-10-28 11:09:31 +0100

    Update WaitForDying.cs
    REMOVE override functions that no longer make sense with the WaitFor changes.

--- Assets/Scripts/Coroutines/WaitFor/WaitForDying.cs
+++ Assets/Scripts/Coroutines/WaitFor/WaitForDying.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 
 namespace PimPamPum
 {
     public class WaitForDying : WaitForDecision
     {
-
         private Func<bool> dying;
 
         public override bool MoveNext()
@@ -17,45 +16,5 @@ namespace PimPamPum
             dying = () => base.MoveNext() && pc.IsDying;
             dyingCorutine = this;
         }
-
-        public override void MakeDecisionCard(Decision decision, Card card = null)
-        {
-            WaitFor waitFor;
-            if ((waitFor = Current as WaitFor) != null)
-            {
-                waitFor.MakeDecisionCard(decision, card);
-            }
-            if (decision == Decision.Die)
-            {
-                base.MakeDecisionCard(decision, card);
-            }
-        }
-
-        public override void MakeDecisionPhaseOne(Decision phaseOneOption, int player, Drop dropEnum, int card)
-        {
-            WaitFor waitFor;
-            if ((waitFor = Current as WaitFor) != null)
-            {
-                waitFor.MakeDecisionPhaseOne(phaseOneOption, player, dropEnum, card);
-            }
-            else
-            {
-                base.MakeDecisionPhaseOne(phaseOneOption, player, dropEnum, card);
-            }
-        }
-
-        public override void MakeDecisionCardIndex(int card)
-        {
-            WaitFor waitFor;
-            if ((waitFor = Current as WaitFor) != null)
-            {
-                waitFor.MakeDecisionCardIndex(card);
-            }
-            else
-            {
-                base.MakeDecisionCardIndex(card);
-            }
-        }
-
     }
 }
